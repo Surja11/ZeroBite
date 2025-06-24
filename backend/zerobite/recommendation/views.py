ea9d1641bef840bc1ad7a52dcd14cb1e133b6a84
Surja11 <stha.surja123@gmail.com>
2025-06-24 18:55:56 +0545

    Added recommendation logic

--- backend/zerobite/recommendation/views.py
+++ backend/zerobite/recommendation/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from product.models import *
+from rest_framework.views import APIView
+from .services.recommendationalgo import *
 
 # Create your views here.
+
+def build_documents(products):
+  documents = {}
+  for product in products:
+    text = f"{product.name} {product.description} {product.category.name} {product.tags} {product.brand}"
+    documents[str(product.id)]  = text
+  return documents
+
+class ProductRecommendationView(APIView):
+  def get(self, request, product_id):
+    products = Product.objects.all()
+    documents = build_documents(products)
+
+    tfidf = TFIDF(documents)
+    tfidf.compute_tfidf()
+
+    recommendations = recommend_similar_products(product_id, tfidf)
+
+    
