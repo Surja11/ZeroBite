521aef19bd98c82d020acf6107a678bc2b7b6990
Surja11 <stha.surja123@gmail.com>
2025-10-30 13:53:19 +0545

    Managed recommendation

--- backend/zerobite/recommendation/serializers.py
+++ backend/zerobite/recommendation/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import  serializers
 from product.models import *
+from datetime import date
 
 class ProductRecommendationSerializer(serializers.ModelSerializer):
   similarity = serializers.FloatField(read_only = True)
   score = serializers.FloatField(read_only = True)
+  discounted_price = serializers.SerializerMethodField()
 
   class Meta:
     model = Product
-    fields = ['id',  'business','category', 'name' ,'price' ,'image' ,'manufactured_date','expiry_date','available','brand', 'view_count', 'similarity','score']
+    fields = ['id',  'business','category', 'name' ,'price' ,'image' ,'manufactured_date','expiry_date','available','brand', 'view_count', 'similarity','score','discounted_price']
+    read_only_fields = ('business', 'discounted_price',)
+  
+
+  def get_discounted_price(self, obj):
+    days_to_expiry = (obj.expiry_date - date.today()).days 
+    if days_to_expiry< 2:
+      return obj.price * 0.4
+    elif days_to_expiry<4:
+      return obj.price *0.65
+    elif days_to_expiry<7:
+      return obj.price * 0.8
+    elif days_to_expiry<11:
+      return obj.price * 0.9
+    return obj.price
+
     
